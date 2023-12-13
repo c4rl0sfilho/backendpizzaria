b56bb67e35547d17970701594bfafb536b2ad551
Carlos Eduardo <ce04435@gmail.com>
2023-12-13 00:24:18 -0300

    aaaaaaaaa

---
+++ app.js
@@ -0,0 +1,106 @@
+/*
+* Autor: Carlos Eduardo
+* Objetivo: Criar o back-end, com API funcional, para uma pizzaria
+* data: 08/12/2023
+* versão: 1.0
+*/
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*')
+
+        response.header('Access-Control-Allow-Methods', 'GET')
+
+        app.use(cors())
+
+        next()
+})
+
+//endpoint: retorna todas as categorias
+app.get('/categorias', cors(), async function(request, response, next){
+    let controleCategorias = require('./module/funcoes.js')
+    let categorias = controleCategorias.getCategorias()
+
+    if(categorias){
+        response.status(200)
+        response.json(categorias)
+    }
+    else{
+        response.status(404)
+        response.json('{erro: item não encontrado}')
+    }
+})
+
+//endpoint: retorna todos os produtos
+app.get('/produtos', cors(), async function(request, response, next){
+    let controleProdutos = require('./module/funcoes.js')
+    let produtos = controleProdutos.getCategorias()
+
+    if(produtos){
+        response.status(200)
+        response.json(produtos)
+    }
+    else{
+        response.status(404)
+        response.json('{erro: item não encontrado}')
+    }
+})
+
+//endpoint: retorna o produto selecionado
+app.get('/produto/', cors(), async function(request, response, next){
+    let nomeProduto = request.query.produto
+
+    let controleProduto = require('./module/funcoes.js')
+    let produto = controleProduto.getProduto(nomeProduto)
+
+    if(produto){
+        response.status(200)
+        response.json(produto)
+    }
+    else{
+        response.status(404)
+        response.json('{erro: item não encontrado}')
+    }
+
+})
+
+//endpoint: retorna dados do cliente selecionado
+app.get('/cliente/', cors(), async function(request, response, next){
+    let nomeCliente = request.query.nome
+
+    let controleClientes = require('./module/funcoes.js')
+    let cliente = controleClientes.getCliente(nomeCliente)
+
+    if(cliente){
+        response.status(200)
+        response.json(cliente)
+    }
+    else{
+        response.status(404)
+        response.json('{erro: item não encontrado}')
+    }
+})
+
+//endpoint: retorna os comentários
+app.get('/comentarios', cors(), async function(request, response, next){
+    let controleComentarios = require('./module/funcoes.js')
+    let comentarios = controleComentarios.getComentarios()
+
+    if(comentarios){
+        response.status(200)
+        response.json(comentarios)
+    }
+    else{
+        response.status(404)
+        response.json('{erro: item não encontrado}')
+    }
+})
+app.listen('8080', function(){
+    console.log('API funcionando!!!!')
+})
---
+++ module/info.js
@@ -0,0 +1,153 @@
+var clientes = { 
+    dados:
+    [
+        {
+            id: '1',
+            nome: 'André',
+            email: 'andre@gmail.com',
+            senha: 'andre1234',
+            telefone: '8511112222', 
+            endereco: 'rua abcd, bairro c, itapevi',
+            cidade: 'Itapevi-SP',
+            img: ''
+        },
+        {
+            id: '2',
+            nome: 'Maria',
+            email: 'maria@gmail.com',
+            senha: 'maria1234',
+            telefone: '1894445555',
+            endereco: 'rua abcd, bairro g, osasco',
+            cidade: 'Osasco-SP',
+            img: ''
+        },
+        {
+            id: '3',
+            nome: 'Vitor',
+            email: 'vitor@gmail.com',
+            senha: 'vitor1234',
+            telefone: '2197778888',
+            endereco: 'rua abcd, bairro q, cotia',
+            cidade: 'Cotia-SP',
+            img: ''
+        },
+        {
+            id: '4',
+            nome: 'Paula',
+            email: 'paula@gmail.com',
+            senha: 'paula123',
+            telefone: '112226666',
+            endereco: 'rua abcd, bairro t, barueri',
+            cidade: 'Barueri-SP',
+            img: ''
+        }
+    ]
+}
+
+var categorias = [
+    {id: '1', nome_categoria: 'Pizza', img_categoria: './img'},
+    {id: '2', nome_categoria: 'Bebida', img_categoria: './img'},
+    {id: '3', nome_categoria: 'Fruta', img_categoria: './img'},
+    {id: '4', nome_categoria: 'Sorvete', img_categoria: './img'}    
+]
+
+var comentarios = {
+    dados:
+    [
+        {
+            id: '1',
+            img_cliente: './img/maria-da-silva.png',
+            nome_cliente: 'Maria da Silva',
+            data_comentario: '15/03/2022',
+            titulo_comentario: 'Pizza muito boa!',
+            conteudo_comentario: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+            numero_estrelas: 4
+        },
+        {
+            id: '2',
+            img_cliente: './img/pedro-gomes-dias.png',
+            nome_cliente: 'Pedro Gomes Dias',
+            data_comentario: '21/05/2022',
+            titulo_comentario: 'Essa pizza é demais, recomendo.',
+            conteudo_comentario: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+            numero_estrelas: 5 
+        }
+
+    ]
+}
+
+var produtos = {
+    dados:
+    [
+        {
+            id: '1',
+            titulo: 'Pizza de Calabreza Com Queijo',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            preco: 'R$ 16,00',
+            imagem: './img/pizzas/PizzaCalabresaQueijo.png',
+            categoria: categorias[0],
+            qtd_estrela: 5
+            
+        },
+        {
+            id: '2',
+            titulo: 'Pizza de Peperoni Com Queijo',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            preco: 'R$ 19,00',
+            imagem: './img/pizzas/PizzaPeperoniQueijo.png',
+            categoria: categorias[0],
+            qtd_estrela: 4
+        },
+        {
+            id: '3',
+            titulo: 'Pizza marguerita',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            preco: 'R$ 18,00',
+            imagem: './img/pizzas/PizzaBrotinhoQueijoTomate.png',
+            categoria: categorias[0],
+            qtd_estrela: 5
+        },
+        {
+            id: '4',
+            titulo: 'Pizza De Peperoni Com Queijo E Tomate',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            preco: 'R$ 19,00',
+            imagem: './img/pizzas/PizzaBrotinhoQueijoTomate.png',
+            categoria: categorias[0],
+            qtd_estrela: 5
+        },
+        {
+            id: '5',
+            titulo: 'Pizza Brotinho Com Queijo e Tomate',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            preco: 'R$ 12,00',
+            imagem: './img/pizzas/PizzaBrotinhoQueijoTomate.png',
+            categoria: categorias[0],
+            qtd_estrela: 4
+        },
+        {
+            id: '6',
+            titulo: 'Coca-Cola 2l',
+            descricao: 'Refrigerante Garrafa 2l Embalagem Econômica',
+            preco: 'R$ 14,99',
+            categoria: categorias[1],
+            qtd_estrela: 5
+        },
+        {
+            id: '7',
+            titulo: 'Cerveja Heineken',
+            descricao: 'Fardo com 6 garrafas de 600ml Embalagem Econômica',
+            preco: 'R$ 44,90',
+            categoria: categorias[1],
+            qtd_estrela: 4
+        }
+
+    ]
+}
+
+module.exports = {
+    clientes,
+    categorias,
+    comentarios,
+    produtos
+}
